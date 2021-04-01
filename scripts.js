33793a35657ff4c5e6144811f9f4480795f94609
Thomas Russell <t.p.russell19@gmail.com>
2021-04-01 12:59:22 -0700

    add more code to scripts.js with comments for clarity
    Co-authored-by: Jamie Knutsen <knutsenjamie@yahoo.com>
    Co-authored-by: Tiffany Greathead <tiffanygreathead@gmail.com>

--- scripts.js
+++ scripts.js
@@ -7,4 +7,19 @@ function convertToInteger(input){
     return false;
   }
 }
-convertToInteger("");
+
+
+function toRoman(number) {
+  const numericalValues = [1, 5, 10, 50, 100, 500, 1000];
+  const romanNumerals = ["I", "V", "X", "L", "C", "D", "M"];
+  if (convertToInteger(number)) { // if convertToInteger(number) is true:
+    for (let i = 0; i < numericalValues.length; i++) { // for each value in [1, 5, 10, 50, 100, 500, 1000]:
+      if (number === numericalValues[i]) { // if number is equal to either 1, 5, 10, 50, 100, 500, or 1000:
+        let romanSymbol = romanNumerals[i]; // then romanSymbol will be assigned to what symbol is at that same index inside of the romanNumerals array
+        return console.log(romanSymbol); // return that symbol
+      }
+    }
+  }
+}
+
+toRoman(5); // will return V becuase 5 is located in the first index of the numericalValues array and the symbol in the first index of the romanNumerals array is "V"
